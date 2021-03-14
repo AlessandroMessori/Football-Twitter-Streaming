ad3d6ae7e9279c7be215134c1ca74f73659f233d
AlessandroMessori <alessandro1.messori@mail.polimi.it>
2021-03-14 16:32:34 +0100

    refactored word and lang count functions, added output stream

--- pySpark/main.py
+++ pySpark/main.py
@@ -2,6 +2,38 @@ from pyspark.sql.session import SparkSession
 from pyspark.sql.functions import explode, split, col, from_json, to_json, json_tuple, window, struct
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, LongType
 
+
+# extracts structured content from json tweet message
+def extractTweetPayload(df, tweetSchema, payloadSchema):
+    return df \
+        .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "CAST(timestamp AS TIMESTAMP)", "offset") \
+        .withColumn("data", from_json("value", tweetSchema)) \
+        .withColumn("payload", from_json("data.payload", payloadSchema)) \
+        .select("payload.*", "key", "timestamp")
+
+# given a steam dataframe returns a df with an additional word count column
+
+
+def wordCountQuery(df, colName):
+    return df \
+        .withWatermark("timestamp", "2 minutes") \
+        .withColumn('word', explode(split(col(colName), ' '))) \
+        .groupBy(window(col("timestamp"), "2 minutes", "1 minutes"),
+                 col('word')
+                 ).count() \
+        .select("word", "count", to_json(struct("word", "count")).alias("value"))
+
+
+def langCountQuery(df, colName):
+    return df \
+        .withWatermark("timestamp", "2 minutes") \
+        .groupBy(
+            window(col("timestamp"), "2 minutes", "1 minutes"),
+            col(colName)
+        ).count() \
+        .select(colName, "count", to_json(struct(colName, "count")).alias("value"))
+
+
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("wordCounter").getOrCreate()
 
@@ -18,43 +50,28 @@ if __name__ == "__main__":
         .readStream \
         .format("kafka") \
         .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("failOnDataLoss", "false") \
         .option("subscribe", "testing2") \
         .option("startingOffsets", "earliest") \
         .load()
 
-    messages = df \
-        .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "CAST(timestamp AS TIMESTAMP)", "offset") \
-        .withColumn("data", from_json("value", tweetSchema)) \
-        .withColumn("payload", from_json("data.payload", payloadSchema)) \
-        .select("payload.*", "key", "timestamp")
+    messages = extractTweetPayload(df, tweetSchema, payloadSchema)
 
-    langCount = messages \
-        .withWatermark("timestamp", "2 minutes") \
-        .groupBy(
-            window(col("timestamp"), "2 minutes", "1 minutes"),
-            col("Lang")
-        ).count() \
-        .select("Lang", "count", to_json(struct("Lang", "count")).alias("value"))
+    wordCount = wordCountQuery(messages, "Text")
 
-    wordCount = messages \
-        .withWatermark("timestamp", "2 minutes") \
-        .withColumn('word', explode(split(col('Text'), ' '))) \
-        .groupBy(window(col("timestamp"), "2 minutes", "1 minutes"),
-                 col('word')
-                 ).count() \
-        .select("word", "count", to_json(struct("word", "count")).alias("value"))
+    #langCount = langCountQuery(messages, "Lang")
 
-    '''query = langCount \
+    query = wordCount \
         .writeStream \
         .format("kafka") \
         .option("checkpointLocation", "/home/alessandro/Desktop/Repos/Football-Twitter-Streaming/checkpoints") \
         .option("kafka.bootstrap.servers", "localhost:9092") \
-        .option("topic", "texts") \
-        .start()'''
+        .option("topic", "words") \
+        .start()
 
-    query = wordCount \
+    '''query = wordCount \
         .writeStream \
         .format("console") \
-        .start()
+        .start()'''
 
     query.awaitTermination()
